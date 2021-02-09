7135490ef16ba8aa16b203d1da5a09cd611716e9
Kosygin <Kosygin-Dmitrii@yandex.ru>
2021-02-09 21:43:33 +0300

    requirements.txt add coverage and pylint adding a test for a function on_event coverage of the main program

--- tests.py
+++ tests.py
@@ -2,14 +2,29 @@
 Mock — это набор объектов, которыми можно подменить настоящий объект.
 На любое обращение к методам, к атрибутам он возвращает тоже mock.
 MOCK - ЗАГЛУШКА
+
+coverage
+coverage run --source=VK_main -m unittset   запускает тестирование с анализом покрытия кода
+coverage report -m    показывает результаты тестирования, покрытие кода в процентах
 """
 
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, ANY
+from vk_api.bot_longpoll import VkBotMessageEvent
 from VK_main import Bot
 
 
 class Test1(TestCase):
+    RAW_EVENT = {'type': 'message_new',
+                 'object': {'message': {'date': 1612890850, 'from_id': 66751578, 'id': 106, 'out': 0,
+                                        'peer_id': 66751578, 'text': 'Привет бот1', 'conversation_message_id': 70,
+                                        'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
+                                        'is_hidden': False},
+                            'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link',
+                                                               'intent_subscribe', 'intent_unsubscribe'],
+                                            'keyboard': True, 'inline_keyboard': True, 'carousel': False,
+                                            'lang_id': 0}},
+                 'group_id': 202361699, 'event_id': 'a5142f5d290238ce7c91e241dc22abb3283a9841'}
     def test_run(self):
         count = 5
         events = [{}]*count  # [{}, {}, ...]
@@ -27,3 +42,28 @@ class Test1(TestCase):
                 bot.on_event.assert_called()  # Вызывали ли метод вообще
                 bot.on_event.assert_any_call({})  # вызывалась функция только с таким аргументом
                 assert bot.on_event.call_count == count
+
+    def test_on_event(self):
+        """
+        test on_event function - создает класс БОТ, с пустыми id and token, мокает методы, взаимодействующуе с
+        внешнимим системами, мокает метод api, позволяющий работать с методами VK, при вызове метода VK send
+        натыкается на заглушку. проверяет, вызывался ли метод send с указанными параметрами. Параметры взяты из
+        сырой строки (raw) класса VkBotMessageEvent из функции инициализации с помощью дебагинга и брейкпоинта.
+
+        :return:
+        """
+
+        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+
+        send_mock = Mock()
+        with patch('VK_main.vk_api.VkApi'):
+            with patch('VK_main.vk_api.bot_longpoll.VkBotLongPoll'):
+                bot = Bot('', '')
+                bot.api = Mock()
+                bot.api.messages.send = send_mock
+
+                bot.on_event(event)
+        send_mock.assert_called_once_with(message='auto answer',
+                                          user_id=self.RAW_EVENT['object']['message']['from_id'],
+                                          random_id=ANY,  # принимает любое значение
+                                          peer_id=self.RAW_EVENT['object']['message']['peer_id'])
